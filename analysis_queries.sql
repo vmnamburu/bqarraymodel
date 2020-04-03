fffef897c4347eac67b73ea88b5a2b72c8d45c1f
Vamsi Namburu <n.vamsimohan@gmail>
2020-04-03 17:36:38 +1100

    Added comments to SQLs

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -3,21 +3,21 @@
 select customer_id
 ,sum(line.item_quantity * line.item_price) customer_total
 from `orders_ds.order_header` hdr 
-join `orders_ds.order_lines` line on (hdr.order_id = line.order_id)
+join `orders_ds.order_lines` line on (hdr.order_id = line.order_id and hdr.order_date = line.order_date)
 group by 1
 
 -- 1.2 Monthly Total
 select date_trunc(hdr.order_date,MONTH) month
 ,sum(line.item_quantity * line.item_price) monthly_total
 from `orders_ds.order_header` hdr 
-join `orders_ds.order_lines` line on (hdr.order_id = line.order_id)
+join `orders_ds.order_lines` line on (hdr.order_id = line.order_id and hdr.order_date = line.order_date)
 group by 1
 
 -- 1.3 Item Total
 select line.item_name
 ,sum(line.item_quantity * line.item_price) monthly_total
 from `orders_ds.order_header` hdr 
-join `orders_ds.order_lines` line on (hdr.order_id = line.order_id)
+join `orders_ds.order_lines` line on (hdr.order_id = line.order_id and hdr.order_date = line.order_date)
 group by 1
 
 -- 2. De-Normalised Data Model
--- create_normalised_tables.sql
+++ create_normalised_tables.sql
@@ -2,6 +2,7 @@ create table `orders_ds.order_header` partition by order_date as
 select order_id,customer_id,order_date
 from `orders_ds.orders`;
 
+--order_date is included at line level to maintain partitioning  
 create table `orders_ds.order_lines` partition by order_date as
 select order_id,order_date,line.line_no,line.item_id,line.item_name,line.item_price,line.item_quantity
 from `orders_ds.orders`, unnest(order_lines) line;
